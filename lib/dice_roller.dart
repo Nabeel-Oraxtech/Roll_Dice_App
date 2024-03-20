872cb4a3ac1d5b20572887bd96e7ca24c56fd501
Nabeel Ahmad <nabeel.oraxtech@gmail.com>
2024-03-20 13:41:42 +0500

    Roll Dice App

---
+++ lib/dice_roller.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var currentDiceRoll = 3;
+  final randimizer = Random();
+
+  void rolldice() {
+    setState(() {
+      currentDiceRoll = randimizer.nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset('assets/images/dice-$currentDiceRoll.png', width: 200),
+        const SizedBox(height: 20),
+        TextButton(
+            onPressed: rolldice,
+            style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(fontSize: 28)),
+            child: const Text('Roll Dice'))
+      ],
+    );
+  }
+}
